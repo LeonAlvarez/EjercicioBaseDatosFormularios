0ab539f9d8c010b4f043470f838966f788be1f70
leon <leon@leon-design.es>
2016-10-14 13:35:55 +0200

    Actualizacion

--- registrar.php
+++ registrar.php
@@ -4,12 +4,25 @@ $username = "root";
 $password = "";
 $dbname = "db1";
 
+$nombre = $_POST["nombre"];
+$apellido1 = $_POST["apellido1"];
+$apellido2 = $_POST["apellido2"];
+$nick = $_POST["nick"];
+$password = $_POST["password"];
+$fechaAlta = date("Y-m-d H:i:s");
+$email = $_POST["email"];
+$domicilio = $_POST["direccion"];
+$poblacion = $_POST["poblacion"];
+$provincia = $_POST["provincia"];
+$nif = $_POST["nif"];
+$telefono = $_POST["telefono"];
+
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "INSERT INTO usuarios (Usuario_nombre, Usuario_apellido1 , Usuario_apellido2, Usuario_nick , Usuario_clave, Usuario_fecha_alta, Usuario_email, Usuario_domicilio, Usuario_poblacion, Usuario_provincia, Usuario_nif, Usuario_numero_telefono )
-    VALUES ('John', 'Doe', 'john@example.com')";
+    VALUES ('$nombre', '$apellido1', '$apellido2', '$nick', '$password', '$fechaAlta', '$email', '$domicilio', '$provincia', '$nif', '$telefono')";
     // use exec() because no results are returned
     $conn->exec($sql);
     //echo "New record created successfully";
